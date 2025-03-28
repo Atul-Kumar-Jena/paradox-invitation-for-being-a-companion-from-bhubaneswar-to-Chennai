d889428786de3f468121c55db2b6324a28ad2642
Atul Kumar Jena <68107352+Atul-Kumar-Jena@users.noreply.github.com>
2025-03-28 23:22:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "paradox-invitation-for-being-a-companion-from-bhubaneswar-to-Chennai",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
